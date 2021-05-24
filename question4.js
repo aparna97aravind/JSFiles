e3043b9407adeda10519585fe87d9a1e5308a1c4
Aparna <aparna.pa@v2solutions.com>
2021-05-24 13:05:36 +0530

    Question4 updated

--- question4.js
+++ question4.js
@@ -1,18 +1,42 @@
-function distinctElemCount(array)
-{
+let array =[10,3,5,1,2,5,3,9,1];
+let outputArr= []; //array storing distinct elements
+let count =0; //to calculate uniqueness
+let flag =false; // to find value is repeating
+
     for(let i=0 ;i<array.length; i++)
     {
-        let count =0;
-        for(let j=0 ; j<array.length; j++)
+      
+        for(let j=0 ; j<outputArr.length; j++)
         {
-            if (array[i] === array[j])
+            if (array[i] === outputArr[j])
             {
-                count++;
+                flag= true; //duplication
             }
+            
         }
-        console.log(`${array[i]} - ${count}`)
-        console.log("\n");
+        count ++;
+        if(count===1 && flag ===false) //distinct element
+        {
+            outputArr.push(array[i]);
+        }
+        flag =false;
+        count =0;
+        
+        
     }
-
-}
-distinctElemCount([10,3,5,1,2,5,3,9,1]);
+    for( let index=0;index<outputArr.length ;index++) // to calcute the repeating count
+    {
+      let val=0;
+      for(let index2=0; index2<array.length; index2++)
+      {
+        if(outputArr[index] === array[index2])
+        {   val++;
+           
+        }
+      }
+      
+      console.log(`${outputArr[index]}- ${val}`);
+      
+    }
+    
+    
