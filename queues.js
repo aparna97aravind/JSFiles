8bee17c4616a502d2faf40f116822f1d9610e87b
Aparna <aparna.pa@v2solutions.com>
2021-05-30 16:18:40 +0530

    updated the alignment of the program

--- queues.js
+++ queues.js
@@ -9,53 +9,50 @@ function queue(size) // function queue with size argument passed.
 
     this.add = function(item)     //enqueue
     {
-            if(this.rear ===  this.size || this.size < 0 )       //isFull condition
+        if(this.rear ===  this.size || this.size < 0 )       //isFull condition
+        {
+            console.log("QUEUE full!!! Cannot Insert elements. Your array is");
+            for(let i=0;i<this.size;i++)
             {
-                console.log("QUEUE full!!! Cannot Insert elements. Your array is");
-                for(let i=0;i<this.size;i++)
-                {
-                    console.log(this.array[i]);          //display array
-                }
+                console.log(this.array[i]);          //display array
+            }
                 
+        }
+        else
+        {
+            if(this.front === 0 && this.rear === 0) // first insertion
+            { 
+                console.log("first Insertion !!");
+                this.array[this.rear] =item;        //element added 
+                console.log(`${item} is added to ${this.rear}th index`);
+                this.rear++;
+                this.front++;  //increment rear and front
+                return (`Rear =  ${this.rear-1}`);
+    
             }
             else
             {
-                if(this.front === 0 && this.rear === 0) // first insertion
-                { 
-                    console.log("first Insertion !!");
-    
-                    this.array[this.rear] =item;        //element added 
-                    console.log(`${item} is added to ${this.rear}th index`);
-                    this.rear++;
-                    this.front++;  //increment rear and front
-                    return (`Rear =  ${this.rear-1}`);
-    
-                }
-                else{
-                    this.array[this.rear] =item;        //element added 
-                    console.log(`${item} is added to ${this.rear}th index`);
-                    this.rear++;  // increment rear
-                    return (`Rear=  ${this.rear-1}`);
-                }
-             }
+                this.array[this.rear] =item;        //element added 
+                console.log(`${item} is added to ${this.rear}th index`);
+                this.rear++;  // increment rear
+                return (`Rear=  ${this.rear-1}`);
+            }
+        }
     }  
                      
    this.remove= function()     //dequeue
     {
-            if(this.front-1 === this.rear) //isEmpty condition
-            {
-                console.log("QUEUE empty!!! Cannot Remove elements");            
-            }
-            else
-            {
-                let removeQItem= this.array[this.front-1];        // item removed stored in a variable and returned       
-                console.log(`${removeQItem} is removed from the Queue`);
-                this.front++;                        //Increment front to get next data
-    
-                return (` removed element ${removeQItem} at position ${this.front-1}`);
-             }
+        if(this.front-1 === this.rear) //isEmpty condition
+        {
+            console.log("QUEUE empty!!! Cannot Remove elements");            
+        }
+        else
+        {
+            let removeElement= this.array[this.front-1];        // item removed stored in a variable and returned       
+            console.log(`${removeElement} is removed from the Queue`);
+            this.front++;                        //Increment front to get next data    
+            return (`removed element ${removeElement} at position ${this.front-1}`);
+        }
     
     }    
 }
-
-const que1= new queue(20);
--- stacks.js
+++ stacks.js
@@ -1,46 +1,41 @@
-function stack(size) // function stack with size argument passed.
+function stack(size)                                               // function stack with size argument passed.
 { 
     this.size = size;
     this.array = []; 
     this.top = 0;
 
-    this.push = function(item)                           //push function
+    this.push = function(element)                                  //push function
         {
-            if(this.top  ===this.size || this.size < 0 )                    //isFull condition
+            if(this.top  ===this.size || this.size < 0 )           //isFull condition
             {
-            console.log("stack full!!! Cannot Insert elements. Your array is");
-            for(let i=0;i<this.size;i++)
-            {
-                console.log(this.array[i]);               //display array
+               console.log("stack full!!! Cannot Insert elements. Your array is");
+               for(let i=0;i<this.size;i++)
+                {
+                        console.log(this.array[i]);               //display array
+                }
+            
             }
-            
-         }
-        else
+            else
+            {
+                this.array[this.top] =element;                    //element added to the top
+                console.log(`${element} is added to ${this.top}th index`);
+                this.top++;                                       //top++
+                return (`top of stack =  ${this.top-1}`);
+            }
+        }  
+
+        this.pop = function()                                     //pop function
         {
-            this.array[this.top] =item;                   //element added to the top
-            
-            console.log(`${item} is added to ${this.top}th index`);
-            
-            this.top++;                                   //top++
-            return (`top of stack =  ${this.top-1}`);
-         }
+              if(this.top === 0)                                  //isEmpty condition
+              {
+                 console.log("stack empty!!! Cannot Remove elements");            
+              }
+              else
+              {
+                let removeElement= this.array[this.top-1];        // item removed stored in a variable and returned       
+                console.log(`${removeElement} is removed from the stack`);
+                this.top--;                                       //decrement top
+                return (`removed Element ${removeElement} from position ${this.top}`);
+              }
+        }    
 }  
-
-        this.pop = function()                             //pop function
-        {
-        if(this.top === 0)                                //isEmpty condition
-        {
-            console.log("stack empty!!! Cannot Remove elements");            
-        }
-        else
-        {
-            let removeItem= this.array[this.top-1];        // item removed stored in a variable and returned       
-            console.log(`${removeItem} is removed from the stack`);
-            this.top--;                                   //decrement top
-
-            return (` removed Element ${removeItem} from position ${this.top}`);
-         }
-}    
-}  
-
-const stack1= new stack(20);
