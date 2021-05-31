c94d9c684a02311eefa2cc65a5ec26a3d3cc1764
Aparna <aparna.pa@v2solutions.com>
2021-05-31 12:24:40 +0530

    Corrections done-Implementing stacks and queues using this keyword

--- queues.js
+++ queues.js
@@ -2,8 +2,8 @@ function queue(size) // function queue with size argument passed.
 {
     this.size = size;
     this.array = [];
-    this.rear=0;
-    this.front=0;
+    this.rear = 0;
+    this.front = 0;
 
 
 
@@ -11,46 +11,36 @@ function queue(size) // function queue with size argument passed.
     {
         if(this.rear ===  this.size || this.size < 0 )       //isFull condition
         {
-            console.log("QUEUE full!!! Cannot Insert elements. Your array is");
-            for(let i=0;i<this.size;i++)
-            {
-                console.log(this.array[i]);          //display array
-            }
+            console.log("QUEUE full!!! Cannot Insert elements");
         }
         else
         {
             if(this.front === 0 && this.rear === 0) // first insertion
             {
-                console.log("first Insertion !!");
-                this.array[this.rear] =item;        //element added 
-                console.log(`${item} is added to ${this.rear}th index`);
+                this.array[this.rear] = item;        //element added 
                 this.rear++;
                 this.front++;  //increment rear and front
-                return (`Rear =  ${this.rear-1}`);
             }
             else
             {
-                this.array[this.rear] =item;        //element added 
-                console.log(`${item} is added to ${this.rear}th index`);
+                this.array[this.rear] = item;        //element added 
                 this.rear++;  // increment rear
-                return (`Rear=  ${this.rear-1}`);
             }
         }
     }
 
 
-    this.remove= function()     //dequeue
+    this.remove = function()     //dequeue
     {
         if(this.front-1 === this.rear) //isEmpty condition
         {
-            console.log("QUEUE empty!!! Cannot Remove elements");            
+            console.log("QUEUE empty!!! Cannot Remove elements");
         }
         else
         {
-            let removeElement= this.array[this.front-1];        // item removed stored in a variable and returned       
-            console.log(`${removeElement} is removed from the Queue`);
+            let removeElement = this.array[this.front];        // item removed stored in a variable and returned
+            this.array.splice(0,1);
             this.front++;                        //Increment front to get next data
-            return (`removed element ${removeElement} at position ${this.front-1}`);
         }
     }
 }
